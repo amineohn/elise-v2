04bb5d2c9e53d1821c78389a6e63e920424467e7
amineohn <amineprojet7@gmail.com>
2022-01-10 10:15:14 +0100

    feat: react-hot-toast

--- pages/send.tsx
+++ pages/send.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { Firebase } from "../libs/firebase";
+import toast, { Toaster } from "react-hot-toast";
 import { Data } from "../libs/types";
 const Home: NextPage = () => {
   const [value, setValue] = useState("");
@@ -60,34 +61,22 @@ const Home: NextPage = () => {
   };
   return (
     <>
-      <button
-        className="fixed bottom-0 right-0 m-4 p-2 border border-black text-black transition rounded-2xl shadow-lg cursor-pointer"
-        onClick={download}
-      >
-        <svg
-          className="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 640 512"
-        >
-          <path
-            fill="currentColor"
-            d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4z"
-          ></path>
-        </svg>
-      </button>
       <div className="h-screen my-48 scale">
-        {success && (
-          <div className="flex justify-center">
-            <div className="text-center text-green-500">{success}</div>
-          </div>
-        )}
-        {error && (
-          <div className="flex justify-center">
-            <div className="text-center text-red-500">{error}</div>
-          </div>
-        )}
-
+        <Toaster />
+        {success && <>{toast.success(success)}</>}
+        {error && <>{toast.error(error)}</>}
         <div className="flex flex-col py-5 px-1 space-y-2">
+          <svg
+            className="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 512"
+            onClick={download}
+          >
+            <path
+              fill="currentColor"
+              d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4z"
+            ></path>
+          </svg>
           {downloaded && (
             <div className="fixed left-0 bottom-0 m-4 p-8 bg-pink-800 shadow-md hover:shodow-lg rounded-2xl max-w-xl animate-heartbeat">
               <div className="flex items-center justify-between">
