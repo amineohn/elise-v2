bc87751a635c1287d258707e3736b6b795119f83
amineohn <amineprojet7@gmail.com>
2022-01-10 14:33:03 +0100

    refactor: bubble buttons, refactor: arrow left like button.

--- pages/send.tsx
+++ pages/send.tsx
@@ -65,61 +65,22 @@ const Home: NextPage = () => {
     <>
       {success && <Toaster />}
       {error && <Toaster />}
-      <div className="h-screen my-48 scale">
+      <div className="border border-black w-10 h-10 ml-6 mt-5 rounded-full px-2.5 py-2">
+        <svg
+          className="fill-current text-black w-5 h-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          onClick={() => router.push("/chose")}
+        >
+          <path
+            fill="currentColor"
+            d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"
+          />
+        </svg>
+      </div>
+      <div className="h-screen my-20 scale">
         <div className="flex flex-col py-5 px-1 space-y-2">
-          <svg
-            className="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 512"
-            onClick={download}
-          >
-            <path
-              fill="currentColor"
-              d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4z"
-            ></path>
-          </svg>
-          {downloaded && (
-            <div className="fixed left-0 bottom-0 m-4 p-8 bg-pink-800 shadow-md hover:shodow-lg rounded-2xl max-w-xl animate-heartbeat">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-16 h-16 rounded-2xl p-3 border border-pink-800 text-pink-400 bg-pink-900"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
-                  <div className="flex flex-col ml-3">
-                    <div className="font-medium leading-none text-neutral-100">
-                      nice gg
-                    </div>
-                    <p className="text-sm text-pink-200 leading-none mt-1">
-                      woohooo you did it
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
           <div className="flex justify-center">
-            <svg
-              className="fill-current text-black w-5 h-5 mt-2 mr-1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              onClick={() => router.push("/chose")}
-            >
-              <path
-                fill="currentColor"
-                d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"
-              />
-            </svg>
             <h1 className="text-center font-bold text-2xl">Saisir un poids</h1>
           </div>
           <div className="flex justify-center items-center">
