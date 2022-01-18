279ec7b06b1d58938492c9658524da862ba821e4
amineohn <amineprojet7@gmail.com>
2022-01-18 12:04:24 +0100

    refactor: send2

--- pages/send2.tsx
+++ pages/send2.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import React, { FormEvent, useEffect, useState } from "react";
 import { Firebase } from "../libs/firebase";
 import toast, { Toaster } from "react-hot-toast";
-import { Data } from "../libs/types";
-import Loading from "../components/loading";
 import { Authentification } from "../libs/authentification";
 const Home: NextPage = () => {
   const [value, setValue] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
-  const [downloaded, setDownload] = useState(false);
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([{}] as any);
   const fire = new Firebase();
@@ -81,39 +77,12 @@ const Home: NextPage = () => {
     }
     setLoading(false);
   };
-  const router = useRouter();
-  const download = async () => {
-    try {
-      const csvData = await data.map((item: Data) => {
-        return `${item.value}`;
-      });
-      const csv = new Blob([csvData], {
-        type: "text/csv",
-      });
-      const url = URL.createObjectURL(csv);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = "data.csv";
-      a.click();
-      URL.revokeObjectURL(url);
-      setDownload(true);
-      toast.success("Fichier téléchargé");
-    } catch (error) {
-      setDownload(false);
-      toast.error("Erreur lors du téléchargement");
-      console.log(error);
-    }
-    setInterval(() => {
-      setDownload(false);
-    }, 5000);
-  };
   const text = `Papier > Benne 2 > ${data} kg`;
 
   return (
     <>
       {success && <Toaster />}
       {error && <Toaster />}
-      {downloaded && <Toaster />}
 
       <div className="h-screen my-10 scale">
         <div className="flex flex-col py-5 px-1 space-y-3">
