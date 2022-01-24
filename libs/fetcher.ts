39ccf73bffa3769d784761a288e53caf5f87bdc8
amineohn <amineprojet7@gmail.com>
2022-01-24 09:13:32 +0100

    use string

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 export default async function fetcher<JSON = any>(
-  input: any,
+  input: string,
   init?: Omit<AxiosRequestConfig, "baseURL" | "method" | "data">
 ): Promise<JSON> {
   const res = await axios.get(input, init);
