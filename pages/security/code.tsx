8c33e4029be7ac2ec3d6cf74ddaffadab334fdc5
isveny <amineprojet7@gmail.com>
2022-01-26 14:48:55 +0100

    feat: added smart keyboard to code page.

--- pages/security/code.tsx
+++ pages/security/code.tsx
@@ -58,25 +58,205 @@ const Code = () => {
       <div className="w-full min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
         <div className="w-full sm:max-w-md p-5 mx-auto">
           <form method="POST" onSubmit={handleSubmit} className="scale">
-            <div className="mb-4">
-              <label className="block mb-1" htmlFor="code">
-                Code
-              </label>
-              <input
-                id="code"
-                type="text"
-                name="code"
-                onChange={(e) => setCode(e.target.value)}
-                className="py-2 px-3 border border-gray-300 focus:border-slate-300 focus:outline-none focus:ring focus:ring-slate-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full bg-white text-black placeholder:to-black transition"
-              />
-            </div>
-            <div className="mt-6">
-              <button
-                type="submit"
-                className="w-full inline-flex items-center justify-center px-4 py-2 bg-slate-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-slate-700 active:bg-slate-700 focus:outline-none focus:border-slate-700 focus:ring focus:ring-slate-200 disabled:opacity-25 transition"
-              >
-                Valider
-              </button>
+            <div className="flex justify-center items-center">
+              <div className="flex flex-col space-y-2">
+                <div className="mb-4">
+                  <label className="block mb-1" htmlFor="code">
+                    Code
+                  </label>
+                  <input
+                    id="code"
+                    type="text"
+                    name="code"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    className="py-2 px-3 border border-gray-300 focus:border-slate-300 focus:outline-none focus:ring focus:ring-slate-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full text-black placeholder:to-black transition bg-white"
+                  />
+                </div>
+                <br />
+                <div>
+                  <div className="flex justify-center items-center">
+                    <div className="grid grid-cols-1 space-y-2">
+                      <div className="space-x-1">
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 hover:bg-slate-70 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "1")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                1
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "2")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                2
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "3")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                3
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                      <div className="space-x-1">
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "4")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                4
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "5")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                5
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "6")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                6
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                      <div className="space-x-1">
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "7")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                7
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "8")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                8
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "9")}
+                          >
+                            <div className="my-5">
+                              <span className="text-center text-3xl font-bold">
+                                9
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                      <div className="space-x-1">
+                        {code.length > 0 ? (
+                          <button className="mr-0.5" type="button">
+                            <div
+                              className="rounded-full w-20 h-20 bg-red-500 transition-colors"
+                              onClick={() => setCode("")}
+                            >
+                              <div className="p-5">
+                                <span className="text-3xl font-bold text-white">
+                                  A
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                        ) : (
+                          <button className="mr-0.5" type="button" disabled>
+                            <div
+                              className="rounded-full w-20 h-20 bg-neutral-300 transition-colors"
+                              onClick={() => setCode("")}
+                            >
+                              <div className="p-5">
+                                <span className="text-3xl font-bold text-white">
+                                  A
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                        )}
+                        <button type="button">
+                          <div
+                            className="rounded-full w-20 h-20 border dark:text-white dark:bg-slate-800 bg-slate-100 hover:bg-slate-700 border-transparent dark:border-transparent"
+                            onClick={() => setCode(code + "0")}
+                          >
+                            <div className="p-5">
+                              <span className="text-center text-3xl font-bold">
+                                0
+                              </span>
+                            </div>
+                          </div>
+                        </button>
+                        {code.length > 0 ? (
+                          <button type="submit">
+                            <div className="rounded-full w-20 h-20 bg-green-500 transition-colors">
+                              <div className="p-5">
+                                <span className=" text-3xl font-bold text-white">
+                                  V
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                        ) : (
+                          <button type="submit" disabled>
+                            <div className="rounded-full w-20 h-20 bg-neutral-300 transition-colors">
+                              <div className="p-5">
+                                <span className=" text-3xl font-bold text-white">
+                                  V
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </form>
         </div>
