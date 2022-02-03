ced9e0f1f9ce0ed8728dc66a0043c9237f5c3db9
amineohn <amineprojet7@gmail.com>
2022-02-03 15:05:58 +0100

    more

--- pages/api/cache.ts
+++ pages/api/cache.ts
@@ -58,6 +58,58 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         console.log(e);
       }
     });
+    fire.collection("test3").onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        value: doc.data().value,
+        ...doc.data(),
+      }));
+      const mapped = data.map((item) => item.value * 1);
+      const total = mapped.reduce((acc, cur) => acc + cur, 0);
+      const stringifyJson = data.map((item) => item.value);
+      try {
+        fs.chmodSync(`./cache/data3.json`, 0o777);
+        fs.writeFileSync(
+          `./cache/data3.json`,
+          JSON.stringify(
+            {
+              data: stringifyJson,
+              total: total + "kg",
+            },
+            null,
+            2
+          )
+        );
+      } catch (e) {
+        console.log(e);
+      }
+      fire.collection("test4").onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          value: doc.data().value,
+          ...doc.data(),
+        }));
+        const mapped = data.map((item) => item.value * 1);
+        const total = mapped.reduce((acc, cur) => acc + cur, 0);
+        const stringifyJson = data.map((item) => item.value);
+        try {
+          fs.chmodSync(`./cache/data4.json`, 0o777);
+          fs.writeFileSync(
+            `./cache/data2.json`,
+            JSON.stringify(
+              {
+                data: stringifyJson,
+                total: total + "kg",
+              },
+              null,
+              2
+            )
+          );
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    });
   }, 15000); // 15s
 
   res.status(200).json({
