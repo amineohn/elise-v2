9f08b8458b718fb5918cffd0f165d84676ee7614
amineohn <amineprojet7@gmail.com>
2022-02-10 11:00:06 +0100

    prevent users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,7 @@ export default function MyApp({
   Component: NextPage;
   pageProps: any;
 }) {
+  const [show, setShow] = useState(true);
   /* const date = new Date();
   const hours = date.getHours();
   const minute = date.getMinutes();
@@ -122,6 +123,86 @@ export default function MyApp({
             }}
           />
           <ThemeProvider defaultTheme="light" attribute="class">
+            <Transition
+              show={show}
+              enter="transition ease-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition ease-in duration-500 slide-out-top"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0 duration-500 slide-out-top"
+              className="z-50"
+            >
+              <div className="bg-rose-600">
+                <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                  <div className="flex items-center justify-between flex-wrap">
+                    <div className="w-0 flex-1 flex items-center">
+                      <span className="flex p-2 rounded-lg bg-rose-800">
+                        <svg
+                          className="h-6 w-6 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+                          />
+                        </svg>
+                      </span>
+                      <p className="ml-3 font-medium text-white truncate">
+                        <span className="md:hidden">
+                          Cette application est en cours de test. Merci de
+                          prévenir Jerome.
+                        </span>
+                        <span className="hidden md:inline">
+                          Cette application est en cours de test. Si vous
+                          rencontrez un problème, n'hésitez pas à contacter
+                          Jerome.
+                        </span>
+                      </p>
+                    </div>
+                    <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto hidden">
+                      <a
+                        href="#"
+                        className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-rose-600 transition bg-white hover:bg-indigo-50"
+                      >
+                        Learn more
+                      </a>
+                    </div>
+                    <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                      <button
+                        type="button"
+                        onClick={() => setShow(false)}
+                        className="-mr-1 flex p-2 rounded-md hover:bg-rose-500 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+                      >
+                        <span className="sr-only">Dismiss</span>
+
+                        <svg
+                          className="h-6 w-6 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
             <div className="flex justify-between py-3 px-3">
               {router.pathname !== "/" && (
                 <div className="flex justify-center">
