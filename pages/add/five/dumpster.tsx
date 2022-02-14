1baad8603b8167612b53f789f70ef6c5e62d1881
amineohn <amineprojet7@gmail.com>
2022-02-14 09:22:44 +0100

    the end.

--- pages/add/five/dumpster.tsx
+++ pages/add/five/dumpster.tsx
@@ -96,7 +96,7 @@ const Home: NextPage = () => {
     return (
       <div>
         <div
-          className="hover:scale-105 hover:transform transition w-72 ml-0 lg:ml-4 max-w-xl h-36 border-2 border-transparent rounded-tl-none rounded-tr-none border-l-slate-900 border-b-slate-900 border-r-slate-900 dark:border-l-white dark:border-b-white dark:border-r-white flex justify-between rounded"
+          className="w-72 ml-0 lg:ml-4 max-w-xl h-36 border-2 border-transparent rounded-tl-none rounded-tr-none border-l-slate-900 border-b-slate-900 border-r-slate-900 dark:border-l-white dark:border-b-white dark:border-r-white flex justify-between rounded"
           onClick={() => {
             router.push(directory);
             if (typeof window !== "undefined") {
